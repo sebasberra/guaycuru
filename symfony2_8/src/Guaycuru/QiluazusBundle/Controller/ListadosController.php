75d02684783607d1b9c8d2d8053d3451717eb004
Sebastian Berra <sberra@santafe.gov.ar>
2016-07-05 10:09:27 -0300

    primera consulta con joins

--- symfony2_8/src/Guaycuru/QiluazusBundle/Controller/ListadosController.php
+++ symfony2_8/src/Guaycuru/QiluazusBundle/Controller/ListadosController.php
@@ -50,5 +50,46 @@ class ListadosController extends Controller
         
     }
     
+    public function camasAction(
+            $id_efector,
+            $estado,
+            $idClasificacionCama)
+    {
+        
+        $qb = $this->getDoctrine()->getManager()->createQuery(
+                'SELECT c, h, s, e
+                 FROM DBHmi2Bundle:Camas AS c
+                 INNER JOIN c.idHabitacion h
+                 INNER JOIN h.idSala s 
+                 INNER JOIN c.idEfector e
+                 INNER JOIN c.idClasificacionCama cc
+                 WHERE c.idEfector = :id_efector
+                   AND c.estado = :estado
+                   AND c.idClasificacionCama = :idClasificacionCama
+                 ORDER BY c.idCama DESC');
+        
+        $qb->setParameter('id_efector',$id_efector);
+        $qb->setParameter('estado',$estado);
+        $qb->setParameter('idClasificacionCama',$idClasificacionCama);
+        
+        //$result = $qb->getResult();
+                
+        //dump($result);die();
+              
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $qb, /* query NOT result */
+            1/*page number*/,
+            10/*limit per page*/
+        );
+        
+        
+        
+        return $this->render(
+                'QiluazusBundle:Default:camas.html.twig', 
+                array('pagination' => $pagination));
+        
+    }
+    
 }
 
